c4141b318dae296327be54a516acdc44a1b3ca78
Rasmus Taulborg Hummelmose <rth@wallmob.com>
2016-04-13 15:01:14 +0200

    Example utilization

---
+++ POSConnectorV2Example.js
@@ -0,0 +1,10 @@
+(function () {
+
+    if (POSConnector === undefined) {
+        console.log("POSConnector is required for the example to run");
+        return;
+    }
+
+    console.log("POSConnector: " + POSConnector);
+
+})();
